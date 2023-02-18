945218f8dcc595bc5c78b848aa24daa166d1947f
Tiago <tiagocasa@gmail.com>
2023-02-17 22:34:40 -0300

    Adicionado IF/Else

--- lib/main.dart
+++ lib/main.dart
@@ -25,6 +25,34 @@ class QuizPage extends StatefulWidget {
 }
 
 class _QuizPageState extends State<QuizPage> {
+  List<Widget> scoreKeeper = [];
+  List<String> questions = [
+    'You can lead a cow down stairs but not up stairs.',
+    'Approximately one quarter of human bones are in the feet.',
+    'A slug\'s blood is green.',
+  ];
+
+  List<bool> correctAnswer = [true, false, true];
+  int questionNumber = 0;
+
+  void CorrectAnswer() {
+    scoreKeeper.add(
+      Icon(
+        Icons.check,
+        color: Colors.green,
+      ),
+    );
+  }
+
+  void WrongAnswer() {
+    scoreKeeper.add(
+      Icon(
+        Icons.close,
+        color: Colors.red,
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -37,7 +65,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                'This is where the question text will go.',
+                questions[questionNumber],
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -60,7 +88,16 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
-                //The user picked true.
+                setState(
+                  () {
+                    if (correctAnswer[questionNumber] == true) {
+                      CorrectAnswer();
+                    } else {
+                      WrongAnswer();
+                    }
+                    questionNumber++;
+                  },
+                );
               },
             ),
           ),
@@ -78,12 +115,23 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
-                //The user picked false.
+                setState(
+                  () {
+                    if (correctAnswer[questionNumber] == false) {
+                      CorrectAnswer();
+                    } else {
+                      WrongAnswer();
+                    }
+                    questionNumber++;
+                  },
+                );
               },
             ),
           ),
         ),
-        //TODO: Add a Row here as your score keeper
+        Row(
+          children: scoreKeeper,
+        )
       ],
     );
   }
